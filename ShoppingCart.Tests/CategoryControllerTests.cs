5c344d98f92d465a0c77f1d2e2c5ace496331403
alisateterieva <alisa.teterieva@nure.ua>
2024-06-06 22:56:24 +0300

    Добавление папки TpRZLab3-master

---
+++ ShoppingCart.Tests/CategoryControllerTests.cs
@@ -0,0 +1,137 @@
+﻿//using Moq;
+//using ShoppingCart.DataAccess.Repositories;
+//using ShoppingCart.DataAccess.ViewModels;
+//using ShoppingCart.Models;
+//using ShoppingCart.Tests.Datasets;
+//using System.Collections.Generic;
+//using ShoppingCart.Web.Areas.Admin.Controllers;
+//using System.Linq.Expressions;
+//using Xunit;
+
+//namespace ShoppingCart.Tests
+//{
+//    public class CategoryControllerTests
+//    {
+//        [Fact]
+//        public void GetCategories_All_ReturnAllCategories()
+//        {
+//            // Arrange
+//            Mock<ICategoryRepository> repositoryMock = new Mock<ICategoryRepository>();
+
+//            repositoryMock.Setup(r => r.GetAll(It.IsAny<string>()))
+//                .Returns(() => CategoryDataset.Categories);
+//            var mockUnitOfWork = new Mock<IUnitOfWork>();
+//            mockUnitOfWork.Setup(uow => uow.Category).Returns(repositoryMock.Object);
+//            var controller = new CategoryController(mockUnitOfWork.Object);
+
+//            // Act
+//            var result = controller.Get();
+
+//            // Assert
+//            Assert.Equal(CategoryDataset.Categories, result.Categories);
+//        }
+//        // Перевірка функції отримання конкретної категорії
+//        [Fact]
+//        public void GetCategory_ById_ReturnCategory()
+//        {
+//            // Arrange
+//            var category = new Category { Id = 1, Name = "Test Category" };
+//            Mock<ICategoryRepository> repositoryMock = new Mock<ICategoryRepository>();
+
+//            repositoryMock.Setup(r => r.GetT(It.IsAny<Expression<Func<Category, bool>>>()))
+//                .Returns(category);
+//            var mockUnitOfWork = new Mock<IUnitOfWork>();
+//            mockUnitOfWork.Setup(uow => uow.Category).Returns(repositoryMock.Object);
+//            var controller = new CategoryController(mockUnitOfWork.Object);
+
+//            // Act
+//            var result = controller.Get(1);
+
+//            // Assert
+//            Assert.Equal(category, result.Category);
+//        }
+//        // Перевірка функції додавання категорії
+//        [Fact]
+//        public void CreateUpdate_ValidCategory_AddsCategory()
+//        {
+//            // Arrange
+//            var categoryVM = new CategoryVM { Category = new Category { Id = 0, Name = "New Category" } };
+//            Mock<ICategoryRepository> repositoryMock = new Mock<ICategoryRepository>();
+
+//            repositoryMock.Setup(r => r.Add(It.IsAny<Category>()));
+//            var mockUnitOfWork = new Mock<IUnitOfWork>();
+//            mockUnitOfWork.Setup(uow => uow.Category).Returns(repositoryMock.Object);
+
+//            var controller = new CategoryController(mockUnitOfWork.Object);
+
+//            // Act
+//            controller.CreateUpdate(categoryVM);
+
+//            // Assert
+//            repositoryMock.Verify(r => r.Add(It.IsAny<Category>()), Times.Once);
+//            mockUnitOfWork.Verify(uow => uow.Save(), Times.Once);
+//        }
+//        // Перевірка функції оновлення категорії
+//        [Fact]
+//        public void CreateUpdate_ExistingCategory_UpdatesCategory()
+//        {
+//            // Arrange
+//            var categoryVM = new CategoryVM { Category = new Category { Id = 1, Name = "Updated Category" } };
+//            Mock<ICategoryRepository> repositoryMock = new Mock<ICategoryRepository>();
+
+//            repositoryMock.Setup(r => r.Update(It.IsAny<Category>()));
+//            var mockUnitOfWork = new Mock<IUnitOfWork>();
+//            mockUnitOfWork.Setup(uow => uow.Category).Returns(repositoryMock.Object);
+
+//            var controller = new CategoryController(mockUnitOfWork.Object);
+
+//            // Act
+//            controller.CreateUpdate(categoryVM);
+
+//            // Assert
+//            repositoryMock.Verify(r => r.Update(It.IsAny<Category>()), Times.Once);
+//            mockUnitOfWork.Verify(uow => uow.Save(), Times.Once);
+//        }
+//        // Перевірка функції видалення конкретної категорії - правильний айді
+//        [Fact]
+//        public void DeleteData_ValidId_DeletesCategory()
+//        {
+//            // Arrange
+//            var category = new Category { Id = 1, Name = "Test Category" };
+//            Mock<ICategoryRepository> repositoryMock = new Mock<ICategoryRepository>();
+
+//            repositoryMock.Setup(r => r.GetT(It.IsAny<Expression<Func<Category, bool>>>()))
+//                .Returns(category);
+//            repositoryMock.Setup(r => r.Delete(It.IsAny<Category>()));
+//            var mockUnitOfWork = new Mock<IUnitOfWork>();
+//            mockUnitOfWork.Setup(uow => uow.Category).Returns(repositoryMock.Object);
+
+//            var controller = new CategoryController(mockUnitOfWork.Object);
+
+//            // Act
+//            controller.DeleteData(1);
+
+//            // Assert
+//            repositoryMock.Verify(r => r.Delete(It.IsAny<Category>()), Times.Once);
+//            mockUnitOfWork.Verify(uow => uow.Save(), Times.Once);
+//        }
+//        // Перевірка функції видалення конкретної категорії - неправильний айді
+//        [Fact]
+//        public void DeleteData_InvalidId_ThrowsException()
+//        {
+//            // Arrange
+//            Mock<ICategoryRepository> repositoryMock = new Mock<ICategoryRepository>();
+
+//            repositoryMock.Setup(r => r.GetT(It.IsAny<Expression<Func<Category, bool>>>()))
+//                .Returns((Category)null);
+//            var mockUnitOfWork = new Mock<IUnitOfWork>();
+//            mockUnitOfWork.Setup(uow => uow.Category).Returns(repositoryMock.Object);
+
+//            var controller = new CategoryController(mockUnitOfWork.Object);
+
+//            // Act & Assert
+//            var exception = Assert.Throws<Exception>(() => controller.DeleteData(1));
+//            Assert.Equal("Category not found", exception.Message);
+//        }
+//    }
+//}
---
+++ ShoppingCart.Tests/OrderControllerTests.cs
@@ -0,0 +1,124 @@
+﻿//using Moq;
+//using ShoppingCart.DataAccess.Repositories;
+//using ShoppingCart.DataAccess.ViewModels;
+//using ShoppingCart.Models;
+//using ShoppingCart.Tests.Datasets;
+//using System.Collections.Generic;
+//using ShoppingCart.Web.Areas.Admin.Controllers;
+//using System.Linq.Expressions;
+//using Xunit;
+//using ShoppingCart.Utility;
+//using Stripe;
+
+//namespace ShoppingCart.Tests
+//{
+//    public class CategoryControllerTests
+//    {
+//        [Fact]
+//        public void OrderDetails_ReturnsOrderDetails()
+//        {
+//            // Arrange
+//            var orderId = 1;
+//            var orderHeader = new OrderHeader { Id = orderId };
+//            var orderDetails = new List<OrderDetail> { new OrderDetail { Id = 1 }, new OrderDetail { Id = 2 } };
+//            var unitOfWorkMock = new Mock<IUnitOfWork>();
+//            unitOfWorkMock.Setup(uow => uow.OrderHeader.GetT(It.IsAny<Func<OrderHeader, bool>>(), It.IsAny<string>()))
+//                .Returns(orderHeader);
+//            unitOfWorkMock.Setup(uow => uow.OrderDetail.GetAll(It.IsAny<string>()))
+//                .Returns(orderDetails);
+//            var controller = new OrderController(unitOfWorkMock.Object);
+
+//            // Act
+//            var result = controller.OrderDetails(orderId);
+
+//            // Assert
+//            Assert.Equal(orderHeader, result.OrderHeader);
+//            Assert.Equal(orderDetails, result.OrderDetails);
+//        }
+
+//        [Fact]
+//        public void SetToInProcess_UpdatesOrderStatusToInProcess()
+//        {
+//            // Arrange
+//            var orderId = 1;
+//            var orderVM = new OrderVM { OrderHeader = new OrderHeader { Id = orderId } };
+//            var unitOfWorkMock = new Mock<IUnitOfWork>();
+//            var controller = new OrderController(unitOfWorkMock.Object);
+
+//            // Act
+//            controller.SetToInProcess(orderVM);
+
+//            // Assert
+//            unitOfWorkMock.Verify(uow => uow.OrderHeader.UpdateStatus(orderId, OrderStatus.StatusInProcess), Times.Once);
+//            unitOfWorkMock.Verify(uow => uow.Save(), Times.Once);
+//        }
+
+//        [Fact]
+//        public void SetToShipped_UpdatesOrderStatusToShipped()
+//        {
+//            // Arrange
+//            var orderId = 1;
+//            var orderVM = new OrderVM { OrderHeader = new OrderHeader { Id = orderId } };
+//            var unitOfWorkMock = new Mock<IUnitOfWork>();
+//            var controller = new OrderController(unitOfWorkMock.Object);
+
+//            // Act
+//            controller.SetToShipped(orderVM);
+
+//            // Assert
+//            unitOfWorkMock.Verify(uow => uow.OrderHeader.Update(It.IsAny<OrderHeader>()), Times.Once);
+//            unitOfWorkMock.Verify(uow => uow.Save(), Times.Once);
+//        }
+
+//        [Fact]
+//        public void SetToCancelOrder_CancelsOrder()
+//        {
+//            // Arrange
+//            var orderId = 1;
+//            var orderHeader = new OrderHeader { Id = orderId, PaymentStatus = PaymentStatus.StatusApproved };
+//            var orderVM = new OrderVM { OrderHeader = orderHeader };
+//            var unitOfWorkMock = new Mock<IUnitOfWork>();
+//            unitOfWorkMock.Setup(uow => uow.OrderHeader.GetT(It.IsAny<Func<OrderHeader, bool>>()))
+//                .Returns(orderHeader);
+//            var controller = new OrderController(unitOfWorkMock.Object);
+
+//            // Act
+//            controller.SetToCancelOrder(orderVM);
+
+//            // Assert
+//            unitOfWorkMock.Verify(uow => uow.OrderHeader.UpdateStatus(orderId, OrderStatus.StatusCancelled), Times.Once);
+//            unitOfWorkMock.Verify(uow => uow.Save(), Times.Once);
+//        }
+
+//        [Fact]
+//        public void SetToCancelOrder_WithApprovedPayment_CreatesRefund()
+//        {
+//            // Arrange
+//            var orderId = 1;
+//            var paymentIntentId = "pi_123456789";
+//            var orderHeader = new OrderHeader { Id = orderId, PaymentStatus = PaymentStatus.StatusApproved, PaymentIntentId = paymentIntentId };
+//            var orderVM = new OrderVM { OrderHeader = orderHeader };
+//            var refundOptions = new RefundCreateOptions
+//            {
+//                Reason = RefundReasons.RequestedByCustomer,
+//                PaymentIntent = paymentIntentId
+//            };
+//            var refundServiceMock = new Mock<RefundService>();
+//            refundServiceMock.Setup(service => service.Create(It.IsAny<RefundCreateOptions>()));
+//            var unitOfWorkMock = new Mock<IUnitOfWork>();
+//            unitOfWorkMock.Setup(uow => uow.OrderHeader.GetT(It.IsAny<Func<OrderHeader, bool>>()))
+//                .Returns(orderHeader);
+//            var controller = new OrderController(unitOfWorkMock.Object);
+
+//            // Act
+//            controller.SetToCancelOrder(orderVM);
+
+//            // Assert
+//            unitOfWorkMock.Verify(uow => uow.OrderHeader.UpdateStatus(orderId, OrderStatus.StatusCancelled), Times.Once);
+//            unitOfWorkMock.Verify(uow => uow.Save(), Times.Once);
+//            refundServiceMock.Verify(service => service.Create(refundOptions), Times.Once);
+//        }
+
+//    }
+
+//}
